50a142b80845aba28f1a748ff4ced10d72a8d7d1
Michal Morawiec <m.morawiec1995@gmail.com>
2017-11-20 20:38:47 +0100

    Added class Drawer

---
+++ src/python_project/Drawer.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+
+class Drawer():
+
+    def __init__(self,y_pred, y_test, target_names, i,X_test,eigenfaces):
+        self.y_pred=y_pred
+        self.y_test=y_test
+        self.target_names=target_names
+        self.i=i
+        self.X_test=X_test
+
+    def plot_gallery(self,images, titles, h, w, n_row=3, n_col=4):
+        """Helper function to plot a gallery of portraits"""
+        plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
+        plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.35)
+        for i in range(n_row * n_col):
+            plt.subplot(n_row, n_col, i + 1)
+            plt.imshow(images[i].reshape((h, w)), cmap=plt.cm.gray)
+            plt.title(titles[i], size=12)
+            plt.xticks(())
+            plt.yticks(())
+
+
+    # plot the result of the prediction on a portion of the test set
+
+    def title(self):
+        pred_name = self.target_names[self.y_pred[self.i]].rsplit(' ', 1)[-1]
+        true_name = self.target_names[self.y_test[self.i]].rsplit(' ', 1)[-1]
+        return 'predicted: %s\ntrue:      %s' % (pred_name, true_name)
+
+
+    def show(self,h,w):
+        prediction_titles = [self.title(self.y_pred, self.y_test, self.target_names, i)
+                             for i in range(self.y_pred.shape[0])]
+
+        self.plot_gallery(self.X_test, prediction_titles, h, w)
+
+        # plot the gallery of the most significative eigenfaces
+
+        eigenface_titles = ["eigenface %d" % i for i in range(self.eigenfaces.shape[0])]
+        self.plot_gallery(self.eigenfaces, eigenface_titles, h, w)
+        plt.show()
--- src/python_project/SVM.py
+++ src/python_project/SVM.py
@@ -41,6 +41,7 @@ from sklearn.metrics import confusion_matrix
 from sklearn.decomposition import PCA
 from sklearn.svm import SVC
 from Data_importer import Data_importer
+from Drawer import Drawer
 
 print(__doc__)
 
@@ -56,7 +57,6 @@ lfw_people = data_importer.import_data(min_faces_per_person=70, resize=0.4)
 
 # introspect the images arrays to find the shapes (for plotting)
 n_samples, h, w = lfw_people.images.shape
-
 # for machine learning we use the 2 data directly (as relative pixel
 # positions info is ignored by this model)
 X = lfw_people.data
@@ -91,8 +91,8 @@ print("Extracting the top %d eigenfaces from %d faces"
 t0 = time()
 pca = PCA(n_components=n_components, svd_solver='randomized',
           whiten=True).fit(X_train)
-print("done in %0.3fs" % (time() - t0))
 
+print("done in %0.3fs" % (time() - t0))
 eigenfaces = pca.components_.reshape((n_components, h, w))
 
 print("Projecting the input data on the eigenfaces orthonormal basis")
@@ -131,35 +131,35 @@ print(confusion_matrix(y_test, y_pred, labels=range(n_classes)))
 # #############################################################################
 # Qualitative evaluation of the predictions using matplotlib
 
-def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
-    """Helper function to plot a gallery of portraits"""
-    plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
-    plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.35)
-    for i in range(n_row * n_col):
-        plt.subplot(n_row, n_col, i + 1)
-        plt.imshow(images[i].reshape((h, w)), cmap=plt.cm.gray)
-        plt.title(titles[i], size=12)
-        plt.xticks(())
-        plt.yticks(())
-
-
-# plot the result of the prediction on a portion of the test set
-
-def title(y_pred, y_test, target_names, i):
-    pred_name = target_names[y_pred[i]].rsplit(' ', 1)[-1]
-    true_name = target_names[y_test[i]].rsplit(' ', 1)[-1]
-    return 'predicted: %s\ntrue:      %s' % (pred_name, true_name)
-
-prediction_titles = [title(y_pred, y_test, target_names, i)
-                     for i in range(y_pred.shape[0])]
-
-plot_gallery(X_test, prediction_titles, h, w)
+# def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
+#     """Helper function to plot a gallery of portraits"""
+#     plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
+#     plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.35)
+#     for i in range(n_row * n_col):
+#         plt.subplot(n_row, n_col, i + 1)
+#         plt.imshow(images[i].reshape((h, w)), cmap=plt.cm.gray)
+#         plt.title(titles[i], size=12)
+#         plt.xticks(())
+#         plt.yticks(())
+#
+#
+# # plot the result of the prediction on a portion of the test set
+#
+# def title(y_pred, y_test, target_names, i):
+#     pred_name = target_names[y_pred[i]].rsplit(' ', 1)[-1]
+#     true_name = target_names[y_test[i]].rsplit(' ', 1)[-1]
+#     return 'predicted: %s\ntrue:      %s' % (pred_name, true_name)
+#
+# prediction_titles = [title(y_pred, y_test, target_names, i)
+#                      for i in range(y_pred.shape[0])]
+#
+# plot_gallery(X_test, prediction_titles, h, w)
 
 # plot the gallery of the most significative eigenfaces
 
 eigenface_titles = ["eigenface %d" % i for i in range(eigenfaces.shape[0])]
-plot_gallery(eigenfaces, eigenface_titles, h, w)
-
+# plot_gallery(eigenfaces, eigenface_titles, h, w)
+drawer = Drawer(y_pred, y_test, target_names, i,X_test,eigenfaces )
+drawer.show()
 data_importer.clear_cache()
 
-plt.show()
